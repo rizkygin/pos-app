7cc3bbaae754d11f42e2cc2f7eeab9405b2b9fee
rizkygin <rizkygin@gmail.com>
2026-04-21 18:02:11 +0700

    add sidebar done

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -3,18 +3,82 @@ import {
     SidebarContent,
     SidebarFooter,
     SidebarGroup,
+    SidebarGroupLabel,
     SidebarHeader,
+    SidebarMenu,
+    SidebarMenuItem,
+    SidebarMenuButton,
+    SidebarMenuBadge,
+    SidebarMenuAction,
 } from "@/components/ui/sidebar"
+
+import { IdCard, Building2, LayoutGrid, SunDim, Moon, LetterText, MailCheck, AlarmCheckIcon } from 'lucide-react';
+
+
+import {
+    Collapsible,
+    CollapsibleContent,
+    CollapsibleTrigger,
+} from "@/components/ui/collapsible"
+
+import { ChevronDown } from 'lucide-react';
+import AppLogo from './app-logo';
+import Link from 'next/link';
+
+
+const projects = [
+    { name: "Order", url: "/dashboard/order", icon: Building2 },
+    { name: "Product", url: "/dashboard/product", icon: LayoutGrid },
+    { name: "Category", url: "/dashboard/category", icon: LetterText },
+    { name: "User", url: "/dashboard/user", icon: IdCard },
+    { name: "Setting", url: "/dashboard/setting", icon: SunDim },
+]
 
 export function AppSidebar() {
     return (
-        <Sidebar>
-            <SidebarHeader />
-            <SidebarContent>
-                <SidebarGroup />
-                <SidebarGroup />
+
+        <Sidebar collapsible="icon" variant="inset">
+            <SidebarHeader>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <SidebarMenuButton size="lg" asChild>
+                            <Link href="/dashboard">
+                                <AppLogo />
+                            </Link>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarHeader>
+            <SidebarContent className="flex flex-col gap-2 mx-2">
+                <SidebarGroup>
+                    <SidebarGroupLabel>Application</SidebarGroupLabel>
+                    <SidebarMenu>
+                        {projects.map((project) => {
+                            const Icon = project.icon;
+                            return (
+                                <SidebarMenuItem key={project.name}>
+                                    <SidebarMenuButton asChild>
+                                        <Link href={project.url}>
+                                            <Icon />
+                                            <span>{project.name}</span>
+                                        </Link>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+                            );
+                        })}
+                    </SidebarMenu>
+                </SidebarGroup>
             </SidebarContent>
-            <SidebarFooter />
+            <SidebarFooter>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <SidebarMenuButton>
+                            Username
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarFooter>
         </Sidebar>
+
     )
 }
